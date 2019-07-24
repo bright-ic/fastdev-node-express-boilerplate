99e54378ee98f429b59cfa8d16ac199b781a08d5
Bright Onwukwe <onwukweb@gmail.com>
2019-07-24 13:29:44 +0100

    Update www.js

--- src/bin/www.js
+++ src/bin/www.js
@@ -7,7 +7,7 @@ import app from "../app";
 import debugLib from "debug";
 import http from "http";
 
-const debug = debugLib("App:server");
+const debug = debugLib("app:server");
 
 /**
  * Get port from environment and store in Express.
